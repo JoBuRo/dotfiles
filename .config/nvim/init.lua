6e10e639e49b0d9d43c33387b5c26b65a560dfe9
Johannes Herrmann <johannes.r.herrmann@gmail.com>
2026-03-02 20:48:55 +0100

    Add neovim config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,4 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+if not vim.g.vscode then
+  require("config.lazy")
+end
---
+++ .config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,13 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+local nvim_tmux_nav = require("nvim-tmux-navigation")
+map("n", "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
+map("n", "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
+map("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
+map("n", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
+map("n", "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
+map("n", "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
